553ebbca25cfaf1728f42661049886c019607ca3
HappyHappy1996 <semenrikirovi4@gmail.com>
2018-04-02 23:02:36 +0300

    using params

--- src/main/java/lab1/BookBuyerAgent.java
+++ src/main/java/lab1/BookBuyerAgent.java
@@ -1,20 +1,41 @@
 package lab1;
 
+import jade.core.AID;
 import jade.core.Agent;
 
-// "C:\Program Files\Java\jdk-9.0.1\bin\javac.exe" -cp jade.jar lab1.BookBuyerAgent.java
-// java -cp jade.jar jade.Boot -gui Peter:examples.hello.HelloWorldAgent
+// to create custom agent you need to make artifact and add it to module libraries
+// program arguments -gui buyer:lab1.BookBuyerAgent
+public class BookBuyerAgent extends Agent {
 
-// -gui -gui bb:examples.bookTrading.lab1.BookBuyerAgent(f.m.dostoyevsky_-_crime_and_punishment)
-public class  BookBuyerAgent extends Agent {
+    // The title of the book to buy
+    private String targetBookTitle;
+    // The list of known seller agents
+    private AID[] sellerAgents = {
+            new AID("seller1", AID.ISLOCALNAME),
+            new AID("seller2", AID.ISLOCALNAME)
+    };
 
+    // Put agent initializations here
     protected void setup() {
         // Printout a welcome message
-        System.out.println("Hello! Buyer-agent" + getAID().getName() + " is ready.");
+        System.out.println("Hello! Buyer-agent " + getAID().getName() + " is ready.");
+
+        // Get the title of the book to buy as a start-up argument
+        Object[] args = getArguments();
+        if (args != null && args.length > 0) {
+            targetBookTitle = (String) args[0];
+            System.out.println("Trying to buy " + targetBookTitle);
+        } else {
+            // Make the agent terminate immediately
+            System.out.println("No book title specified");
+            doDelete();
+        }
     }
 
-    public static void main(String[] args) {
-        System.out.println("Hello!");
+    // Put agent clean-up operations here
+    protected void takeDown() {
+        // Printout a dismissal message
+        System.out.println("Buyer-agent " + getAID().getName() + "terminating.");
     }
 
 }
